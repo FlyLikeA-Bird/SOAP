32db3b5cdd16982ae1f4219a7e1502bba38ee3a0
Waris MANSOOR <97187532+FlyLikeA-Bird@users.noreply.github.com>
2026-02-11 16:22:34 +0100

    TP1

--- src/MonServiceWeb.java
+++ src/MonServiceWeb.java
@@ -1,1 +1,18 @@
+import javax.jws.WebService;
 
+@WebService(targetNamespace = "http://www.polytech.fr")
+public class MonserviceWeb {
+
+  @WebMethod(operationName = "Convertir")
+  public double conversion(double mt){
+    return mt*0.9;
+  }
+  public double somme(@WebParam(name = "parametre1") double a, double b){
+    return a+b;
+  }
+  
+  public Etudiant getEtudiant(int identifiant){
+    return new Etudiant(1, " luigi",  19);
+    
+  }
+}
